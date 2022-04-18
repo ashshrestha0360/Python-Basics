ce7751fbb9d0ee0f222f0a86a13969e8bc018de1
ashshrestha0360 <103939076+ashshrestha0360@users.noreply.github.com>
2022-04-18 19:24:27 +0545

    Add files via upload

---
+++ conditional.py
@@ -0,0 +1,28 @@
+# age = int(input("Enter your age"))
+#if else
+# if(age >= 18):
+#     print("You can cast vote")
+# else:
+#     print("You cannot cast vote")
+#if else ladder
+# day = int(input("Enter the day"))
+# if(day == 1):
+#     print("It can be Sunday")
+# elif(day == 2):
+#     print("It can be Monday")
+# else:
+#     print("It can be Holiday")
+# num = 2
+# if not num == 2:
+#     print("This is if statement")
+# else:
+#     print("This is else statement")
+num1 = 7
+num2 = 5
+num3 = 6
+if num1 > num2 and num1 > num3:
+    print("num1 is greatest")
+elif num2 > num1 and num2 > num3:
+    print("num2 is greatest")
+else:
+    print("num3 is greatest")
